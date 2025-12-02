40fdc53a966b3063767c58731441c25f459c7b11
Yash Patel <yashpatel1815@hotmail.com>
2025-12-02 12:05:06 -0500

    Finished day 2

--- src/Day2.java
+++ src/Day2.java
@@ -5,13 +5,15 @@ import java.util.Vector;
 
 public class Day2 {
     public static void main(String[] args) {
-        Day2.solvePart1();
+        Day2.solvePart2();
     }
 
     public static void solvePart1() {
         Scanner sc;
         String allIDs;
         String[] tokenizedIDRanges;
+        long sum = 0;
+
         // Get inputs
         try {
             sc = new Scanner(new File("./res/day2.txt"));
@@ -25,13 +27,97 @@ public class Day2 {
 
         // Check ranges
         // 101 is a valid id
+        // variable sliding window
         for (String range: tokenizedIDRanges) {
             // get left and right numbers of the range
             long left = Long.parseLong(range.split("-")[0]);
             long right = Long.parseLong(range.split("-")[1]);
-            for (long i = left; i <= right; i++) {
 
+            for (long i = left; i <= right; i++) {
+                String number = String.valueOf(i);
+                char[] numberArray = number.toCharArray();
+
+                // Only need to deal with numbers that have an even number of digits
+                if (numberArray.length % 2 != 0) continue;
+
+                int start = 0, end = numberArray.length/2 - 1;
+                boolean hasRepetition = true;
+                while (end < numberArray.length-1) {
+                    if (numberArray[start] == numberArray[end + 1]) {
+                        start++;
+                        end++;
+                    } else {
+                        hasRepetition = false;
+                        break;
+                    }
+                }
+
+                if (hasRepetition) sum += Long.parseLong(number);
             }
         }
+
+        System.out.println(sum);
+    }
+
+    public static void solvePart2() {
+        Scanner sc;
+        String allIDs;
+        String[] tokenizedIDRanges;
+        long sum = 0;
+
+        // Get inputs
+        try {
+            sc = new Scanner(new File("./res/day2.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Split the ranges
+        allIDs = sc.nextLine();
+        tokenizedIDRanges = allIDs.split(",");
+
+        // Check ranges
+        // 101 is a valid id
+        // variable sliding window
+        for (String range: tokenizedIDRanges) {
+            // get left and right numbers of the range
+            long left = Long.parseLong(range.split("-")[0]);
+            long right = Long.parseLong(range.split("-")[1]);
+
+            for (long i = left; i <= right; i++) {
+                String number = String.valueOf(i);
+                char[] numberArray = number.toCharArray();
+                int nTimesRepeating = 2;
+
+                while (nTimesRepeating <= numberArray.length) {
+                    if (numberArray.length % nTimesRepeating != 0) {
+                        nTimesRepeating++;
+                        continue;
+                    }
+
+                    // check if there are nTimesRepeating numbers
+                    boolean hasRepetition = true;
+                    int start = 0;
+                    int end = numberArray.length / nTimesRepeating - 1;
+                    while (end < numberArray.length-1) {
+                        if (numberArray[start] == numberArray[end + 1]) {
+                            start++;
+                            end++;
+                        } else {
+                            hasRepetition = false;
+                            break;
+                        }
+                    }
+                    nTimesRepeating++;
+                    // If it has at least 2 repetitions, go to the next number.
+                    if (hasRepetition) {
+                        sum += Long.parseLong(number);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(sum);
     }
 }
